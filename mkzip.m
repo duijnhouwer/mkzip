b3e6a8f53bfc54d7a1258a157884cecb2f8159b6
Jacob Duijnhouwer <j.duijnhouwer@gmail.com>
2023-09-06 16:49:38 -0400

    Update mkzip.m
    Remove class types from properties list (e.g. @double). This is no longer used or acceptable in MATLAB class definitions

--- mkzip.m
+++ mkzip.m
@@ -21,11 +21,11 @@ classdef mkzip
         data=[];
     end
     properties (GetAccess=public,SetAccess=protected)
-        zipBytes@double;
-        dataClass@char;
-        dataSize@double;
-        dataBytes@double;
-        ratio@double;
+        zipBytes
+        dataClass
+        dataSize
+        dataBytes
+        ratio
     end
     methods
         function Z=mkzip(M)
@@ -167,19 +167,19 @@ function s=bytesToStr(b,forceunit)
     tera=kilo*giga;
     peta=kilo*tera;
     exa=kilo*peta;
-    if b<kilo && dyn || strcmpi(forceunit,'B');
+    if b<kilo && dyn || strcmpi(forceunit,'B')
         s=[num2str(b) ' Byte'];
-    elseif b>=kilo && b<mega  && dyn || strcmpi(forceunit,'kB') || strcmpi(forceunit,'k');
+    elseif b>=kilo && b<mega  && dyn || strcmpi(forceunit,'kB') || strcmpi(forceunit,'k')
         s=[num2str(b/kilo,'%.2f') ' kB'];
-    elseif b>=mega && b<giga && dyn|| strcmpi(forceunit,'MB') || strcmpi(forceunit,'M');
+    elseif b>=mega && b<giga && dyn|| strcmpi(forceunit,'MB') || strcmpi(forceunit,'M')
         s=[num2str(b/mega,'%.2f') ' MB'];
-    elseif b>=giga && b<tera && dyn|| strcmpi(forceunit,'GB') || strcmpi(forceunit,'G');
+    elseif b>=giga && b<tera && dyn|| strcmpi(forceunit,'GB') || strcmpi(forceunit,'G')
         s=[num2str(b/giga,'%.2f') ' GB'];
-    elseif b>=tera && b<peta && dyn|| strcmpi(forceunit,'TB') || strcmpi(forceunit,'T');
+    elseif b>=tera && b<peta && dyn|| strcmpi(forceunit,'TB') || strcmpi(forceunit,'T')
         s=[num2str(b/tera,'%.2f') ' TB'];
-    elseif b>=peta && b<exa && dyn|| strcmpi(forceunit,'PB') || strcmpi(forceunit,'P');
+    elseif b>=peta && b<exa && dyn|| strcmpi(forceunit,'PB') || strcmpi(forceunit,'P')
         s=[num2str(b/peta,'%.2f') ' PB'];
-    elseif b>=exa && dyn || strcmpi(forceunit,'EB') || strcmpi(forceunit,'E');
+    elseif b>=exa && dyn || strcmpi(forceunit,'EB') || strcmpi(forceunit,'E')
         s=[num2str(b/exa,'%.2f') ' EB'];
     else
         s=[num2str(b) ' B'];
